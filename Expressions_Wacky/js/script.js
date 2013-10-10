249b27a4bab3ff9c9b7d45640467fb20a7875089
Matthew Tolson <matthew@matthewtolson.com>
2013-10-10 18:42:21 -0400

    Added in Console print out

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -10,5 +10,11 @@ item[1] = prompt("What is your second food item?");
 calories[1] = parseInt(prompt("How many calories are in this item?"));
 fat[1] = parseInt(prompt("How many calories are in this item?"));
 
+var totalCalories = calories[0]+calories[1]
+var totalFat = fat[0]+fat[1]
 
-console.log(item[1])
+console.log("In your "+item[0]+" and "+ item[1]+ "you are consuming "+totalCalories+ " calories and "+ totalFat+" grams of fat.")
+
+totalCalories *= 2
+totalFat *= 2
+console.log("If you have 2 serving of each "+item[0]+" and "+ item[1]+ "you are consuming "+totalCalories+ " calories and "+ totalFat+" grams of fat.")
