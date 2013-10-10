9c58d65a5d18cb67b08d7397faa195986e4f7a61
Matthew Tolson <matthew@matthewtolson.com>
2013-10-10 19:07:15 -0400

    Industry: setup var

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,9 @@
 // Matthew Tolson
-// Date
-// Assignment
+// 10/10/13
+// Expressions: Industry
+
+var screenSize = parseInt(prompt("What is the width of the screen you are building for?\n(in pixels"));
+var margin = parseInt(prompt("What is the margin you want in between the colums and the page edge?\n(in pixels)");
+var column = parseInt(prompt("How many colums do you want?"));
+
+margin = (column+1)*margin
