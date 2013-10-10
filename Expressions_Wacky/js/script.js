7b1b4cc433ea4e35a81341e623e77d1a12b73dd0
Matthew Tolson <matthew@matthewtolson.com>
2013-10-09 21:55:45 -0400

    wacky: fixed errors

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -9,15 +9,11 @@ var eJuiceCost = parseInt(prompt("How much does the oil cost month?"));
 
 var cigCost = cost/20;
 var costMonth = cigCost*numCigs*30;
-var costYear =  costDay*365;
-var eJuiceYear = eJuice*12;
+var costYear =  costMonth*12;
+var eJuiceYear = eJuiceCost*12;
 var ecigTotalCost = eJuiceYear+ecigCost;
 var ecigAvgMonthCost = ecigTotalCost/12;
 
-console.log("You spend $"+costDay+" per month in cigarettes and $"+costYear+" per year.");
+console.log("You spend $"+costMonth+" per month in cigarettes and $"+costYear+" per year.");
 console.log("Your average monthly cost for and Ecig is $"+ecigAvgMonthCost+" and yearly cost is $"+ecigTotalCost+".");
-
-
-
-console.log(cigCost);
-console.log(costDay);
+console.log("You can save $"+ (ecigAvgMonthCost-costMonth) +" a month and $"+ecigTotalCost-costYear+" a year by switching to an ecig");
