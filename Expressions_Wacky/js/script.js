c13e41125b66866e471c4dd8e5bdf8bda4fb1ebd
Matthew Tolson <matthew@matthewtolson.com>
2013-10-09 21:50:17 -0400

    Wacky updated formula and added additional formulas

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,11 +4,20 @@
 
 var numCigs = parseInt(prompt("How many cigarettes do you smoke in one day?\n(in the number of cigretts not packs)" ));
 var cost = parseInt(prompt("How much does one pack of cigarettes cost?"));
+var ecigCost = parseInt(prompt("How much does an Ecig cost\n(One time Cost for the equipment)?"));
+var eJuiceCost = parseInt(prompt("How much does the oil cost month?"));
 
-var cigCost = cost/20
-var costDay = cigCost*numCigs
+var cigCost = cost/20;
+var costMonth = cigCost*numCigs*30;
+var costYear =  costDay*365;
+var eJuiceYear = eJuice*12;
+var ecigTotalCost = eJuiceYear+ecigCost;
+var ecigAvgMonthCost = ecigTotalCost/12;
 
-console.log("You spend $"+costDay+" per day in cigarettes and $"+costDay*365+" per year.")
+console.log("You spend $"+costDay+" per month in cigarettes and $"+costYear+" per year.");
+console.log("Your average monthly cost for and Ecig is $"+ecigAvgMonthCost+" and yearly cost is $"+ecigTotalCost+".");
 
-console.log(cigCost)
-console.log(costDay)
+
+
+console.log(cigCost);
+console.log(costDay);
