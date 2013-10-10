2ff80a3c7e426cc293a090ed0f4f636620d1e880
Matthew Tolson <matthew@matthewtolson.com>
2013-10-10 18:27:35 -0400

    wacky added in formulas

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,8 +2,18 @@
 // 10/09/13
 // Expressions: Wacky
 
-var item1 [0] = prompt("What is your first food item?");
-var item1 [1] = parseInt(prompt("How many calories are in this item?"));
-var item1 [2] = parseInt(prompt("How many grams of fat are in this item?"));
+var item1[0] = prompt("What is your first food item?");
+var item1[1] = parseInt(prompt("How many calories are in this item?"));
+var item1[2] = parseInt(prompt("How many grams of fat are in this item?"));
 
+var item2[0] = prompt("What is your first food item?");
+var item2[1] = parseInt(prompt("How many calories are in this item?"));
+var item2[2] = parseInt(prompt("How many grams of fat are in this item?"));
 
+var totalCalories += item1[1] 
+var totalCalories += item2[1]
+
+var totalFat = item1[1] + item2[1]
+
+console.log(totalCalories)
+console.log(totalFat)
