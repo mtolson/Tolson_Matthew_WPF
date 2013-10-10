42dd24e4d80d321495b9a1dcf09bbb94a64be68a
Matthew Tolson <matthew@matthewtolson.com>
2013-10-10 18:54:26 -0400

    Wacky: Added in comments and missed ;

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,19 +2,19 @@
 // 10/09/13
 // Expressions: Wacky
 
-var item = [prompt("What is your first food item?")];
-var calories = [parseInt(prompt("How many calories are in this item?"))];
-var fat = [parseInt(prompt("How many calories are in this item?"))];
+var item = [prompt("What is your first food item?")]; //Gathering the first food item name
+var calories = [parseInt(prompt("How many calories are in this item?"))]; //Asking the number of Calories in the item
+var fat = [parseInt(prompt("How many grams of fat is in this item?"))];//Getting the amount of Fat in the item
 
-item[1] = prompt("What is your second food item?");
-calories[1] = parseInt(prompt("How many calories are in this item?"));
-fat[1] = parseInt(prompt("How many calories are in this item?"));
+item[1] = prompt("What is your second food item?");//Gathering the second food item name
+calories[1] = parseInt(prompt("How many calories are in this item?"));//Asking the number of Calories in the item
+fat[1] = parseInt(prompt("How many grams of fat is in this item?"));//Getting the amount of Fat in the item
 
-var totalCalories = calories[0]+calories[1]
-var totalFat = fat[0]+fat[1]
+var totalCalories = calories[0]+calories[1];//Adding all the Calories to get a total
+var totalFat = fat[0]+fat[1];//adding all grams of fat to get a total fat count
 
-console.log("In your "+item[0]+" and "+ item[1]+ " you are consuming "+totalCalories+ " calories and "+ totalFat+" grams of fat.")
+console.log("In your "+item[0]+" and "+ item[1]+ " you are consuming "+totalCalories+ " calories and "+ totalFat+" grams of fat."); //printing the total amount of calories and grams of fat in both items
 
-totalCalories *= 2
-totalFat *= 2
-console.log("If you have 2 serving of each "+item[0]+" and "+ item[1]+ " you are consuming "+totalCalories+ " calories and "+ totalFat+" grams of fat.")
+totalCalories *= 2; // calculating if there was 2 servings of the items
+totalFat *= 2; // calculating if there was 2 servings of the items
+console.log("If you have 2 serving of each "+item[0]+" and "+ item[1]+ " you are consuming "+totalCalories+ " calories and "+ totalFat+" grams of fat."); //Printing to the console the results if they had two servings of the food. 
