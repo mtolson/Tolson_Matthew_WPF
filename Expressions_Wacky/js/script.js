e20281f48cd0bb0b73cdf182f45829e1f7091e4c
Matthew Tolson <matthew@matthewtolson.com>
2013-10-10 18:43:24 -0400

    Updated Conolse Print out

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -13,8 +13,8 @@ fat[1] = parseInt(prompt("How many calories are in this item?"));
 var totalCalories = calories[0]+calories[1]
 var totalFat = fat[0]+fat[1]
 
-console.log("In your "+item[0]+" and "+ item[1]+ "you are consuming "+totalCalories+ " calories and "+ totalFat+" grams of fat.")
+console.log("In your "+item[0]+" and "+ item[1]+ " you are consuming "+totalCalories+ " calories and "+ totalFat+" grams of fat.")
 
 totalCalories *= 2
 totalFat *= 2
-console.log("If you have 2 serving of each "+item[0]+" and "+ item[1]+ "you are consuming "+totalCalories+ " calories and "+ totalFat+" grams of fat.")
+console.log("If you have 2 serving of each "+item[0]+" and "+ item[1]+ " you are consuming "+totalCalories+ " calories and "+ totalFat+" grams of fat.")
