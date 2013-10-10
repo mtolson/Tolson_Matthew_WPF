f56af0ac818d8e9f85499888e97d4dbecff5892a
Matthew Tolson <matthew@matthewtolson.com>
2013-10-09 21:30:44 -0400

    Wacky: var setup

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,3 +2,11 @@
 // 10/09/13
 // Expressions: Wacky
 
+var numCigs = parseInt(prompt("How many cigarettes do you smoke in one day?\n(in the number of cigretts not packs)" ));
+var cost = parseInt(prompt("How much does one pack of cigarettes cost?"));
+
+var cigCost = cost/20
+var costDay = cigCost*numCigs
+
+console.log(cigCost)
+console.log(costDay)
