b1756dd86e7a63466d9f08e762bbf343408ed024
Matthew Tolson <matthew@matthewtolson.com>
2013-10-10 18:37:54 -0400

    Fixed Bug

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,9 +2,9 @@
 // 10/09/13
 // Expressions: Wacky
 
-var item = prompt("What is your first food item?");
-var calories = parseInt(prompt("How many calories are in this item?"));
-var fat = parseInt(prompt("How many calories are in this item?"));
+var item = [prompt("What is your first food item?")];
+var calories = [parseInt(prompt("How many calories are in this item?"))];
+var fat = [parseInt(prompt("How many calories are in this item?"))];
 
 item[1] = prompt("What is your second food item?");
 calories[1] = parseInt(prompt("How many calories are in this item?"));
