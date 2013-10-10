e981f1ecb151322f499b6423f57faa3bda7b2d49
Matthew Tolson <matthew@matthewtolson.com>
2013-10-10 18:24:19 -0400

    Wacky: Var setup

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,3 +2,8 @@
 // 10/09/13
 // Expressions: Wacky
 
+var item1 [0] = prompt("What is your first food item?");
+var item1 [1] = parseInt(prompt("How many calories are in this item?"));
+var item1 [2] = parseInt(prompt("How many grams of fat are in this item?"));
+
+
