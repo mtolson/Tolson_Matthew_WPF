b046ab1537ce7c79021993b96bca039286152e22
Matthew Tolson <matthew@matthewtolson.com>
2013-10-16 19:31:04 -0400

    added comments

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -3,14 +3,14 @@
 // Conditionals Worksheet
 
 
-var mpg = parseInt(prompt("What is your car's MPG?"));
-var gauge = parseInt(prompt("What percent of gas is left in your tank?"));
-var tank = parseInt(prompt("How big is your gas tank?"));
-var miles = 200
-var gasLeft = tank * (gauge/100)
+var mpg = parseInt(prompt("What is your car's MPG?")); // Ask for the MPG your car gets
+var gauge = parseInt(prompt("What percent of gas is left in your tank?")); // Ask for the the percent of gas left in your car
+var tank = parseInt(prompt("How big is your gas tank?")); // Ask how big your tank is
+var miles = 200 // Sets how many miles you have to drive before the next gas station
+var gasLeft = tank * (gauge/100)// divides the size of the tank by the percent of gas left in your tank to get how many gallons are left
 
-if(gasLeft * mpg > miles){
-  console.log("Yes, you can make it without stopping for gas!");
+if(gasLeft * mpg > miles){ //determins if the amount of gas left in the tank * mpg is greater than the miles needed to be driven
+  console.log("Yes, you can make it without stopping for gas!"); //If tru this prints
 }else{
-  console.log("You only have "+ gasLeft+" gallons of gas in your tank, better get gas now while you can!");
+  console.log("You only have "+ gasLeft+" gallons of gas in your tank, better get gas now while you can!");//if false then this prints
 }
