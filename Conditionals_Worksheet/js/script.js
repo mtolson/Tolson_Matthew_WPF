56dd674b607978af0efb0556179fdd8314401305
Matthew Tolson <matthew@matthewtolson.com>
2013-10-16 19:39:28 -0400

    Worksheet Example 2

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -3,14 +3,28 @@
 // Conditionals Worksheet
 
 
-var mpg = parseInt(prompt("What is your car's MPG?")); // Ask for the MPG your car gets
-var gauge = parseInt(prompt("What percent of gas is left in your tank?")); // Ask for the the percent of gas left in your car
-var tank = parseInt(prompt("How big is your gas tank?")); // Ask how big your tank is
-var miles = 200 // Sets how many miles you have to drive before the next gas station
-var gasLeft = tank * (gauge/100)// divides the size of the tank by the percent of gas left in your tank to get how many gallons are left
+//var mpg = parseInt(prompt("What is your car's MPG?")); // Ask for the MPG your car gets
+//var gauge = parseInt(prompt("What percent of gas is left in your tank?")); // Ask for the the percent of gas left in your car
+//var tank = parseInt(prompt("How big is your gas tank?")); // Ask how big your tank is
+//var miles = 200 // Sets how many miles you have to drive before the next gas station
+//var gasLeft = tank * (gauge/100)// divides the size of the tank by the percent of gas left in your tank to get how many gallons are left
+//
+//if(gasLeft * mpg > miles){ //determins if the amount of gas left in the tank * mpg is greater than the miles needed to be driven
+//  console.log("Yes, you can make it without stopping for gas!"); //If tru this prints
+//}else{
+//  console.log("You only have "+ gasLeft+" gallons of gas in your tank, better get gas now while you can!");//if false then this prints
+//}
 
-if(gasLeft * mpg > miles){ //determins if the amount of gas left in the tank * mpg is greater than the miles needed to be driven
-  console.log("Yes, you can make it without stopping for gas!"); //If tru this prints
+
+var username = prompt("Please enter the correct Username:");
+var password = prompt("Please enter the correct Password:");
+var cUsername = "Tolson"
+var cPassword = "FullSail"
+
+if(username === cUsername && password === cPassword){
+  console.log("Welcome, "+cUsername+"!");
+}else if(username === cUsername){
+  console.log("Password does not match our records.");
 }else{
-  console.log("You only have "+ gasLeft+" gallons of gas in your tank, better get gas now while you can!");//if false then this prints
+  console.log("User not found. Try again.");
 }
