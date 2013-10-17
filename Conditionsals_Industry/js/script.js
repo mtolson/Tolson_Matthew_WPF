ceb1377e84e7c975d0072dbfe3ab72a8388d4a8d
Matthew Tolson <matthew@matthewtolson.com>
2013-10-16 21:00:49 -0400

    Setup

--- Conditionsals_Industry/js/script.js
+++ Conditionsals_Industry/js/script.js
@@ -1,3 +1,20 @@
 // Matthew Tolson
-// Date
-// Assignment
+// 10/16/13
+// Conditionals Industry
+
+var clientStatus = prompt("What is the Client's Rank?\n (1-3)");
+var priority = prompt("Is this an emergency?\n(y or n)");
+
+if (priority ==="y" && clientStatus === "1"){
+  console.log("SLA states you need to respond within 15 mins")
+}else if (priority ==="y" && clientStatus === "2") {
+  console.log("SLA states you need to respond within 30 mins")
+}else if (priority ==="y" && clientStatus === "3") {
+  console.log("SLA states you need to respond within 90 mins")
+}else if (priority ==="n" && clientStatus === "1") {
+  console.log("SLA states you need to respond within 4 hours")
+}else if (priority ==="n" && clientStatus === "2") {
+  console.log("SLA states you need to respond within 8 hours")
+}else {
+  console.log("SLA states you need to respond within 24 hours")
+}
