fd1c4ba79cf8c6106e7d51f860f89a4e61f0e753
Matthew Tolson <matthew@matthewtolson.com>
2013-10-16 20:45:03 -0400

    Initial layout

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,4 +3,9 @@
 // Conditionals Wacky
 
 
+var age = parseInt(prompt("What is your age?");
+var sex = prompt("Are you Male or Female?\n(m or f)");
+var allowed;
+
+allowed = ( sex === "f" && age >=18 || sex ==="m" && age >=21) ? "You can enter the Club!" : "Sorry, You are not allowed inside";
 
