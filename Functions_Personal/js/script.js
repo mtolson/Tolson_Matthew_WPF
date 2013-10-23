e483291691985b4b629cfdfcd2df8e54f2e1d49a
Matthew Tolson <matthew@matthewtolson.com>
2013-10-23 19:55:29 -0400

    Added in promt and if statement

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,12 +2,16 @@
 // 10/23/13
 // Functions Personal
 
-var today = new Date()
-var trip =  new Date("2014/05/13")
+var today = new Date();
+var trip =  new Date(prompt"When is your trip?\n(Format yyyy/mm/dd)");
 
 var daysTillTrip = function(today, trip){
-  return Math.abs(today-trip)/(24*60*60*1000)
+  return Math.abs(today-trip)/(24*60*60*1000);
 }
 
-var days = daysTillTrip(today, trip)
-console.log(days)
+if (today< trip){
+  var days = daysTillTrip(today, trip);
+  console.log("You have "+days+" days until your trip");
+}else {
+  console.log("Your trip has already happened. I hope you had a good time");
+}
