cd4ff466a73e08574948932673034aa48ab30ad4
Matthew Tolson <matthew@matthewtolson.com>
2013-10-23 19:16:24 -0400

    Example 1

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -2,3 +2,11 @@
 // 10/23/13
 // Functions Worksheet
 
+var radius = 4;
+var pi = 3.14;
+
+
+var calculateCircumference = function(r, p){
+  return 2*r*p;
+}
+var circumference = calculateCircumference(radius, pi);
