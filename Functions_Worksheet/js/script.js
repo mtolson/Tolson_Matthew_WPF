e1de167603016162e645f6185bcd094bcbc776e6
Matthew Tolson <matthew@matthewtolson.com>
2013-10-23 19:22:39 -0400

    Example two

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -5,9 +5,19 @@
 var radius = 4;
 var pi = 3.14;
 
-
 var calculateCircumference = function(r, p){
   return 2*r*p;
 }
 var circumference = calculateCircumference(radius, pi);
 console.log("The circumfernce of the circle is " + circumference)
+
+
+var weight = 200;
+
+function calculateBeeStings(w){
+  return w*8.666666667;
+}
+
+var numBeeStings = calculateBeeStings(weight);
+
+console.log("It takes " + numBeeStings+" bee stings to kill this animal.")
