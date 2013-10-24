f1efeddbed9d4bb6de8968274755ca8e34a12403
Matthew Tolson <matthew@matthewtolson.com>
2013-10-24 17:50:57 -0400

    Wacky example

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,3 +1,21 @@
 // Matthew Tolson
-// Date
-// Assignment
+// 10/24/13
+// Functions Wacky
+
+var wallHeight1 = parseInt(prompt("What is the height of the first wall?");
+var wallWidth1 = parseInt(prompt("What is the width of the first wall?");
+
+var wallHeight2 = parseInt(prompt("What is the height of the second wall?");
+var wallWidth2 = parseInt(prompt("What is the width of the second wall?");
+
+var wallHeight3 = parseInt(prompt("What is the height of the third wall?");
+var wallWidth3 = parseInt(prompt("What is the width of the third wall?");
+
+var wallHeight4 = parseInt(prompt("What is the height of the fourth wall?");
+var wallWidth4 = parseInt(prompt("What is the width of the fourth wall?");
+
+
+function calcRoomArea(){
+  
+
+}
