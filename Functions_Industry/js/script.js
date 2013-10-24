d13a1a9da205f0c8166dcb30d051042be1bbdc72
Matthew Tolson <matthew@matthewtolson.com>
2013-10-24 18:28:30 -0400

    Industry setup

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,17 @@
 // Matthew Tolson
-// Date
-// Assignment
+// 10/24/13
+// Functions Industry
+
+var invoiceTotal = parseInt(prompt("What is the Order Total?"));
+var invoiceDate = new Date(prompt("What is the Date the Order was signed?\n (format yyyy/mm/dd)"));
+
+var count = 3;
+var quarterly = invoiceTotal/4;
+console.log(invoiceDate);
+
+do{
+  invoiceDate = invoiceDate + 7776000000;
+  console.log(invoiceDate);
+}
+while (c>0);
+
