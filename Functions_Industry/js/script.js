c14707c17f505893d0cbe614bba87e89014479e8
Matthew Tolson <matthew@matthewtolson.com>
2013-10-24 18:42:11 -0400

    Added comments to industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,17 +2,20 @@
 // 10/24/13
 // Functions Industry
 
-var invoiceTotal = parseInt(prompt("What is the Order Total?"));
-var invoiceDate = new Date(prompt("What is the Date the Order was signed?\n (format yyyy/mm/dd)"));
+var invoiceTotal = parseInt(prompt("What is the Order Total?"));//Gets infomation on the total cost of the order
+var invoiceDate = new Date(prompt("What is the Date the Order was signed?\n (format yyyy/mm/dd)"));//Finds out the first invoice date
 
-var count = 3;
-var quarterly = invoiceTotal/4;
-console.log("Your Invoice due on "+invoiceDate+" is in the amount of $"+quarterly+".");
+var count = 3; //sets a counter to only allow the loop to run three times
 
+var quarterly = invoiceTotal/4; //Finds out the quartlery price
+
+
+console.log("Your Invoice due on "+invoiceDate+" is in the amount of $"+quarterly+".");//Prints the first invoice date and cost.
+//does a loop until the count is less than or equal to zero
 do{
-  invoiceDate.setDate(invoiceDate.getDate()+90);
-  console.log("Your Invoice due on "+invoiceDate+" is in the amount of $"+quarterly+".");
-  count--
+  invoiceDate.setDate(invoiceDate.getDate()+90); // adds 90days to the invoice date to find the next date 
+  console.log("Your Invoice due on "+invoiceDate+" is in the amount of $"+quarterly+"."); //prints the results of the next date and price
+  count-- // subtracts 1 from the count
 }
-while (count>0);
+while (count>0); //checks the count to see if the loop needs to run again.
 
